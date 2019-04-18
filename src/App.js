a5adbed846f0cf1082fc1e13741941dd5c81e3bc
suman rajput <44019903+sumandeepak8@users.noreply.github.com>
2019-04-18 13:16:10 +0530

    Refactor transfer method

--- src/App.js
+++ src/App.js
@@ -2,38 +2,72 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: props.books,
+      bookToAdd: ''
+    };
+  }
 
   transfer() {
-    let bookName = document.getElementsByTagName('input')[0].value;
+    this.state.bookToAdd = document.getElementsByTagName('input')[0].value;
     const reqData = {
       method: 'POST',
-      body: bookName
+      body: this.state.bookToAdd
     };
-
     fetch('/addBook', reqData)
       .then(res => res.json())
-      .then(rows => {
-        let booksTable = document.getElementById('books');
-        let tbody = booksTable.getElementsByTagName('tbody');
-        tbody.innerHTML = '';
-        rows.forEach(element => {
-          let row = document.createElement('tr');
-          let idColoumn = document.createElement('td');
-          idColoumn.innerText = element.id;
-          let nameColoumn = document.createElement('td');
-          nameColoumn.innerText = element.name;
-          row.appendChild(idColoumn);
-          row.appendChild(nameColoumn);
-          tbody[0].appendChild(row);
+      .then(booksData => {
+        this.setState({
+          bookToAdd: this.state.bookToAdd,
+          books: booksData
         });
       });
   }
 
-  render() {
+  getForm() {
     return (
       <div id="form" className="form">
         <input placeholder="Enter the book name" />
         <input type="submit" value="add" onClick={this.transfer.bind(this)} />
+      </div>
+    );
+  }
+
+  getRow(bookId, bookName) {
+    return (
+      <tr>
+        <td>{bookId}</td>
+        <td>{bookName}</td>
+      </tr>
+    );
+  }
+
+  getTableBody() {
+    if (this.state.books != undefined)
+      return this.state.books.map(book => {
+        return this.getRow(book.id, book.name);
+      });
+  }
+
+  getTable() {
+    return (
+      <table id="books" className="books">
+        <thead>
+          <td>BOOK ID</td>
+          <td>BOOK NAME</td>
+        </thead>
+        <tbody>{this.getTableBody()}</tbody>
+      </table>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        {this.getForm()}
+        {this.getTable()}
       </div>
     );
   }
